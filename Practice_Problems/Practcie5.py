07e29acef696aa65d4d877d836e77097f1042096
Aduley7 <pamadu0310@gmail.com>
2026-02-28 00:00:34 +0530

    Added Practice 5, 6 and 7 problems

---
+++ Practice_Problems/Practcie5.py
@@ -0,0 +1,13 @@
+#WAP to ask the user enter the names of their 3 favourite movies and store them in a list
+Movies = []
+Movie1 = input("Enter the name of the first movie:")
+Movie2 = input("Enter the name of the second movie:")
+Movie3 = input("Enter the name of the third movie:")
+
+Movies.append(Movie1)
+Movies.append(Movie2)
+Movies.append(Movie3)
+
+print(Movies)
+
+
---
+++ Practice_Problems/Practice7.py
@@ -0,0 +1,6 @@
+#WAP to count the number of students with the grade "A" in the following tuple.
+
+Grade = ["C","D","A","A","B","B","A"]
+Count = Grade.count("A")
+
+print("The number students having Grade A are", Count)
